15be73d1f480a5a2caf03d3525621e87f201f3d9
mzabrisk <mattzabriskie@matts-air.lan>
2022-12-21 22:05:01 -0700

    updating file ordanization

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -27,7 +27,7 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
 -- generating unique_titles table
-SELECT Distinct on(emp_no)
+SELECT DISTINCT on(emp_no)
 	emp_no,
 	first_name,
 	last_name,
